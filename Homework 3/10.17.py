229ae8cd245399eaa04595309d063c4379beca2c
dandoan1 <72234672+dandoan1@users.noreply.github.com>
2021-06-14 01:18:02 -0500

    Add files via upload
    added 10.17 and wip 10.19

---
+++ Homework 3/10.17.py
@@ -0,0 +1,31 @@
+#Dan Doan 1986920
+class ItemToPurchase:
+  item_name = "none"
+  item_price = 0
+  item_quantity = 0
+  def init(self):
+    self.item_name = ""
+    self.item_price = 0
+    self.item_quantity = 0
+  def print_item_cost(self):
+    print("{} {} @ ${:.0f} = ${:.0f}".format(self.item_name, self.item_quantity, self.item_price, (self.item_price*self.item_quantity)))
+if __name__=="__main__":
+  item = ItemToPurchase()
+  print ("Item 1")
+  name1 = input("Enter the item name:\n")
+  price1 = float(input("Enter the item price:\n"))
+  quantity1 = int(input("Enter the item quantity:\n"))
+  print ("\nItem 2")
+  name2 = input("Enter the item name:\n")
+  price2 = float(input("Enter the item price:\n"))
+  quantity2 = int(input("Enter the item quantity:\n"))
+  print("\nTOTAL COST")
+  item.item_name = name1
+  item.item_price = price1
+  item.item_quantity = quantity1
+  item.print_item_cost()
+  item.item_name = name2
+  item.item_price = price2
+  item.item_quantity = quantity2
+  item.print_item_cost()
+  print("\nTotal: ${:.0f}".format(price1*quantity1+price2*quantity2))
